4782a30201035b71f480065733bcfd3ef74d5b4d
Raíssa <raissajoaquim29@gmail.com>
2025-03-31 10:30:54 -0300

    mudança teste

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,14 +1,13 @@
 const Campground = require('../models/campground');
 
-
-
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render('campgrounds/index', { campgrounds });
 };
 
 module.exports.renderNewForm = (req, res) => {
-    res.render('campgrounds/new');
+   
+  res.render('campgrounds/new');
 };
 
 module.exports.createCampground = async (req, res, next) => {
